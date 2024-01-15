496f376d8c6d82529a67749a814e72ce7855b378
eientei <einstein95@users.noreply.github.com>
2024-01-15 21:36:51 +1300

    Add Asenheim RPA3 header

--- unrpa/versions/unofficial_rpa.py
+++ unrpa/versions/unofficial_rpa.py
@@ -16,5 +16,10 @@ class RPA40(RPA3):
     name = "RPA-4.0"
     header = b"RPA-4.0"
 
+class RPAAsenheim(RPA3):
+    """A slightly custom variant of RPA-3.0."""
+    name = "RPA-Asenheim"
+    # "asenheim" in a substitution cipher
+    header = b"\x01\x13\x05\x0e\x08\x05\x09\x0d"
 
-versions: Tuple[Type[Version], ...] = (RPA32, RPA40,)
+versions: Tuple[Type[Version], ...] = (RPA32, RPA40, RPAAsenheim)
